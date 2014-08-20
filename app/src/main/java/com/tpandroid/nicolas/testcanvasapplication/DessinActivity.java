474c3ab26f9f2812e775cee2a93c1907985254b8
nicolasPecot <nicolas.pecot@gmail.com>
2014-08-21 01:28:29 +0200

    ajout drag n drop activity

--- app/src/main/java/com/tpandroid/nicolas/testcanvasapplication/DessinActivity.java
+++ app/src/main/java/com/tpandroid/nicolas/testcanvasapplication/DessinActivity.java
@@ -2,6 +2,8 @@ package com.tpandroid.nicolas.testcanvasapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.tpandroid.nicolas.testcanvasapplication.util.SystemUiHider;
 
@@ -17,5 +19,23 @@ public class DessinActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dessin);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_dragdrop){
+            // Créer une nouvelle activité pour le drag &
+        }
+        if (item.getItemId() == R.id.menu_draw) {
+            return super.onOptionsItemSelected(item);
+        }
+        return true;
     }
 }
